69d7bee87fbc8d2018226095e89f5d5a806f8864
Dan <danielandrewvoss@gmail.com>
2016-06-06 16:56:05 -0400

    Add logout

--- src/com/dvoss/Main.java
+++ src/com/dvoss/Main.java
@@ -54,5 +54,13 @@ public class Main {
                     return "";
                 }
         );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    user = null;
+                    response.redirect("/");
+                    return "";
+                }
+        );
     }
 }
