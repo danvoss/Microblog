1105b388ba9b5aa950576c8f9a815267d859486c
Dan <danielandrewvoss@gmail.com>
2016-06-06 15:32:18 -0400

    Initial import

---
+++ src/com/dvoss/Main.java
@@ -0,0 +1,46 @@
+package com.dvoss;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
+public class Main {
+
+    static User user;
+
+    public static void main(String[] args) {
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    if (user == null) {
+                        return new ModelAndView(m, "index.html");
+                    }
+                    else {
+                        m.put("name", user.name);
+                    }
+                    return new ModelAndView(m, "");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/create-user",
+                (request, response) -> {
+                    String username = request.queryParams("username");
+                    user = new User(username);
+                    response.redirect("/");
+                    return "";
+                }
+        );
+        Spark.post(
+                "/create-message",
+                (request, response) -> {
+                    return "";
+
+                }
+        );
+    }
+}
---
+++ src/com/dvoss/Message.java
@@ -0,0 +1,12 @@
+package com.dvoss;
+
+/**
+ * Created by Dan on 6/6/16.
+ */
+public class Message {
+    String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+}
